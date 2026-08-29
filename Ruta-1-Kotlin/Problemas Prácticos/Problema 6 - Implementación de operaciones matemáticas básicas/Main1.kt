20861ab8fc7caf9b58df7233d3f79fd4ea18c1dc
JulioDCH <julio.chavezh@unmsm.edu.pe>
2026-08-29 17:50:02 -0500

    Problema 6: Caso 1
    Corrected compilation error in main function to calculate the sum of two numbers.

---
+++ Ruta-1-Kotlin/Problemas Prácticos/Problema 6 - Implementación de operaciones matemáticas básicas/Main1.kt
@@ -0,0 +1,25 @@
+/* Problema 6: Implementación de operaciones matemáticas básicas
+ * En este ejercicio se realizan operaciones matemáticas básicas y se imprime el resultado.
+ *
+ * Paso 1:
+ * La función main() contiene un error de compilación:
+ *
+ * fun main() {
+ *     val firstNumber = 10
+ *     val secondNumber = 5
+ *
+ *     println("$firstNumber + $secondNumber = $result")
+ * }
+ *
+ * ¿Puedes corregir el error para que el programa imprima:
+ * 10 + 5 = 15
+ */
+
+// Solución 1:
+fun main() {
+    val firstNumber = 10
+	  val secondNumber = 5
+    val result = firstNumber + secondNumber
+
+    println("$firstNumber + $secondNumber = $result")
+ }
